6f3bed9fe39027d123e9fb83d55861968634ab8e
MohamedNijas <93792336+MohamedNijas@users.noreply.github.com>
2023-03-26 13:37:48 +0530

    Delete widz directory

--- widz/item/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Category,Item
-# Register your models here.
-admin.site.register(Category)#register the category inside the admin to see the  category in the admin 
-admin.site.register(Item)
--- widz/item/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import detail
-
-app_name = "item"
-
-urlpatterns = [
-    path("<int:pk>",detail,name = "detail")
-]
